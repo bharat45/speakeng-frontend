2ca29950b25e90628e4c4dd11e76a984e817facc
Bharat Rajpal <bharat.rajpal@gripinvest.in>
2024-03-13 11:24:59 +0530

    formatting + removed caching

--- app/components/HeroSection/index.tsx
+++ app/components/HeroSection/index.tsx
@@ -1,45 +1,51 @@
-'use client';
-import Image from 'next/image'
-import styles from './HeroSection.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./HeroSection.module.css";
 
-import whatsappIcon from '@/public/whatsapp.png'
-import { getHeroData } from '@/app/lib/data';
-import { useEffect, useState } from 'react';
+import whatsappIcon from "@/public/whatsapp.png";
+import { getHeroData } from "@/app/lib/data";
+import { useEffect, useState } from "react";
 
 const HeroSection = () => {
+  const [data, setData] = useState<any>({});
 
-    const [data, setData] = useState<any>({});
+  const redirectToWhatsapp = () => {
+    const message = data.data.attributes.whatsappMessage.split(" ").join("%20");
+    window.open(
+      `https://api.whatsapp.com/send?phone=${data.data.attributes.phoneNumber}&text=${message}`,
+      "_blank"
+    );
+  };
 
-    const redirectToWhatsapp = () => {
-        const message = data.data.attributes.whatsappMessage.split(' ').join('%20');
-        window.open(`https://api.whatsapp.com/send?phone=${data.data.attributes.phoneNumber}&text=${message}`, '_blank')
-    }
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getHeroData();
+      setData(data);
+    };
+    getData();
+  }, []);
 
-    useEffect(() => {  
-        const getData = async () => {
-            const data = await getHeroData();
-            setData(data);
-        }
-        getData();
-    }, []); 
-
-    if(!data.data) return null;
+  if (!data.data) return null;
 
   return (
     <div className={styles.container}>
-        <div className={styles.profile}>
-            <img src={data.data.attributes.profilePicture.data.attributes.url} alt="" style={{borderRadius: '50%'}} />
-        </div>
-        <div className={styles.header}>
-            <h1>{data.data.attributes.title}</h1>
-            <h2>{data.data.attributes.tagLine}</h2>
-            <button className={styles.whatsappButton} onClick={redirectToWhatsapp}>
-                <Image src={whatsappIcon} alt="" />
-                <span>Whatsapp</span>
-            </button>
-        </div>
+      <div className={styles.profile}>
+        <img
+          src={data.data.attributes.profilePicture.data.attributes.url}
+          alt=""
+          style={{ borderRadius: "50%" }}
+        />
+      </div>
+      <div className={styles.header}>
+        <h1>{data.data.attributes.title}</h1>
+        <h2>{data.data.attributes.tagLine}</h2>
+        <button className={styles.whatsappButton} onClick={redirectToWhatsapp}>
+          <Image src={whatsappIcon} alt="" />
+          <span>Whatsapp</span>
+        </button>
+      </div>
     </div>
-  )
-}
- 
-export default HeroSection
+  );
+};
+
+export default HeroSection;
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,30 +1,20 @@
-'use server';
+"use server";
 import axios from "axios";
 
 const API_URL = "https://speakeng-backend.onrender.com/api";
 
-var navData: any;
 export const getNavData = async () => {
   try {
-    if (navData) {
-      return navData;
-    }
     const res = await axios.get(API_URL + "/navbar?populate=*");
-    navData = res.data;
     return res.data;
   } catch (error) {
     console.log(error);
   }
 };
 
-var HeroData: any;
 export const getHeroData = async () => {
   try {
-    if (HeroData) {
-      return HeroData;
-    }
     const res = await axios.get(API_URL + "/hero-section?populate=*");
-    HeroData = res.data;
     return res.data;
   } catch (error) {
     console.log(error);
